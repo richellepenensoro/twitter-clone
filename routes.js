9d8c9dbaea9a183695572e9923413ad740d5112c
Arnelle Balane <arnellebalane@gmail.com>
2017-10-24 14:57:11 +0800

    add login and logout routes

--- routes.js
+++ routes.js
@@ -1,7 +1,21 @@
 const router = new require('express').Router();
+const passport = require('./config/passport');
 
 router.get('/', (req, res) => {
+    console.log(req.user);
     res.render('landing.html');
+});
+
+router.post('/login',
+    passport.authenticate('local', {
+        failureRedirect: '/',
+        successRedirect: '/'
+    })
+);
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
