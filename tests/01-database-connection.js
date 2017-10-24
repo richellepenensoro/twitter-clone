7c4afc6e79db12e0c57e1bb3f9cbef1ff6ad7aab
Arnelle Balane <arnellebalane@gmail.com>
2017-10-24 19:31:30 +0800

    update tests to use async/await

--- tests/01-database-connection.js
+++ tests/01-database-connection.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const { Pool } = require('pg');
 const config = require('../config');
 
@@ -6,15 +5,15 @@ const exercisesDirectory = config.get('EXERCISES_DIRECTORY');
 const databaseConfig = require(`../${exercisesDirectory}/01-database-connection.json`);
 
 suite('01-database-connection', function() {
-    test('development database configured properly', function() {
+    test('development database configured properly', async function() {
         const databaseConnection = databaseConfig.development;
         const pool = new Pool(databaseConnection);
-        return pool.connect();
+        await pool.connect();
     });
 
-    test('test database configured properly', function() {
+    test('test database configured properly', async function() {
         const databaseConnection = databaseConfig.test;
         const pool = new Pool(databaseConnection);
-        return pool.connect();
+        await pool.connect();
     });
 });
--- tests/02-create-users-table.js
+++ tests/02-create-users-table.js
@@ -7,31 +7,32 @@ const exercisesDirectory = config.get('EXERCISES_DIRECTORY');
 const databaseConfig = require(`../${exercisesDirectory}/01-database-connection.json`);
 
 suite('02-create-users-table', function() {
-    beforeEach(function() {
+    const dropUsersTableQuery = 'DROP TABLE IF EXISTS users;';
+
+    setup(async function() {
         const databaseConnection = databaseConfig.test;
         this.pool = new Pool(databaseConnection);
-        return this.pool.query('DROP TABLE IF EXISTS users;');
+        await this.pool.query(dropUsersTableQuery);
     });
 
-    afterEach(function() {
-        return this.pool.query('DROP TABLE IF EXISTS users;')
-            .then(() => this.pool.end());
+    teardown(async function() {
+        await this.pool.query(dropUsersTableQuery);
+        await this.pool.end();
     });
 
-    test('creates users table successfully', function() {
-        return query('02-create-users-table.sql').then(sql => {
-            return this.pool.query(sql).then(() => {
-                const getTableQuery = `
-                    SELECT *
-                    FROM information_schema.tables
-                    WHERE table_name = 'users'
-                `;
-                return this.pool.query(getTableQuery).then(results => {
-                    return results.rows.length === 1
-                        ? Promise.resolve()
-                        : Promise.reject('users table does not exist');
-                });
-            });
-        });
+    test('creates users table successfully', async function() {
+        const sql = await query('02-create-users-table.sql');
+        await this.pool.query(sql);
+
+        const getUsersTableQuery = `
+            SELECT *
+            FROM information_schema.tables
+            WHERE table_name = 'users';
+        `;
+        const result = await this.pool.query(getUsersTableQuery);
+
+        if (result.rows.length === 0) {
+            throw new Error('"users" table does not exist');
+        }
     });
 });
