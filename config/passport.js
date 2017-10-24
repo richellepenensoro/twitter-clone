429abf02f8690dadc00acd91f84924e5127f69ac
Arnelle Balane <arnellebalane@gmail.com>
2017-10-24 14:51:27 +0800

    update passport config with serializer and deserializer

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,28 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new LocalStrategy((username, password, done) => {
+passport.use(new LocalStrategy({
+    usernameField: 'identifier'
+}, (username, password, done) => {
     const user = {
+        id: 1,
         username: 'arnellebalane',
         email: 'arnellebalane@gmail.com'
     };
     done(null, user);
 }));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    const user = {
+        id: id,
+        username: 'arnellebalane',
+        email: 'arnellebalane@gmail.com'
+    };
+    done(null, user);
+});
+
+module.exports = passport;
