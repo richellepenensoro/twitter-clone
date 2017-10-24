003c522110bc44b38304f884d620f06ce14b3370
Arnelle Balane <arnellebalane@gmail.com>
2017-10-25 00:07:05 +0800

    initial tests for exercise 4 insert user

---
+++ tests/04-insert-user.js
@@ -0,0 +1,70 @@
+const { Pool } = require('pg');
+const config = require('../config');
+const { query } = require('../utils');
+
+const exercisesDirectory = config.get('EXERCISES_DIRECTORY');
+const databaseConfig = require(`../${exercisesDirectory}/01-database-connection.json`);
+
+suite('04-insert-user', function() {
+    const dropUsersTableQuery = 'DROP TABLE IF EXISTS users;';
+
+    suiteSetup(async function() {
+        this.pool = new Pool(databaseConfig.test);
+        await this.pool.query(dropUsersTableQuery);
+
+        const sql = await query('02-create-users-table.sql');
+        await this.pool.query(sql);
+    });
+
+    suiteTeardown(async function() {
+        await this.pool.query(dropUsersTableQuery);
+        await this.pool.end();
+    });
+
+    test('inserts the user successfully', async function() {
+        const usersToInsert = [{
+            fullname: 'First User',
+            username: 'firstuser',
+            email: 'firstuser@gmail.com',
+            password: 'password'
+        }, {
+            fullname: 'Second User',
+            username: 'seconduser',
+            email: 'seconduser@gmail.com',
+            password: 'password'
+        }];
+
+        for (let userData of usersToInsert) {
+            const insertUserQuery = await query('04-insert-user.sql', userData);
+            await this.pool.query(insertUserQuery);
+
+            const getUsersWithEmailQuery = `
+                SELECT * FROM users
+                WHERE email = '${userData.email}';
+            `;
+            const users = await this.pool.query(getUsersWithEmailQuery);
+
+            if (users.rows.length === 0) {
+                throw new Error('user data was not inserted');
+            }
+
+            const user = users.rows[0];
+            for (let key in userData) {
+                if (userData[key] !== user[key]) {
+                    throw new Error(`user's "${key}" column was not insert properly`);
+                }
+            }
+        }
+    });
+
+    test('sets proper value for "created_at" column', async function() {
+        const userData = {
+            fullname: 'First User',
+            username: 'firstuser',
+            email: 'firstuser@gmail.com',
+            password: 'password'
+        };
+        const insertUserQuery = await query('04-insert-user.sql', userData);
+        await this.pool.query(insertUserQuery);
+    });
+});
