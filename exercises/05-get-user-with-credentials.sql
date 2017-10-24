191330a52503249670c0d48ca8a722bc56e230e2
Arnelle Balane <arnellebalane@gmail.com>
2017-10-24 23:08:47 +0800

    add instructions for exercise 5

---
+++ exercises/05-get-user-with-credentials.sql
@@ -0,0 +1,15 @@
+-- Get the user that have the specifed credentials.
+--
+-- You have access to the following variables which can be used as placeholders
+-- for actual values:
+--     - identifier  (could be email or username)
+--     - password
+--
+-- Using variables (note that these are not SQL variables, they are only used
+-- in order for this exercise to work properly):
+--
+--     given  : password = 'arnellebalane@gmail.com'
+--     query  : SOME AWESOME QUERY password = '{{password}}';
+--     result : SOME AWESOME QUERY password = 'arnellebalane@gmail.com';
+--
+-- Write your query below:
