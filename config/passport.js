174c22f4a4117c5e1ba95a783c6ca4bd293c7227
Arnelle Balane <arnellebalane@gmail.com>
2017-10-24 23:02:13 +0800

    update passport deserializer to perform sql query

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const db = require('./database');
+const { query } = require('../utils');
 
 passport.use(new LocalStrategy({
     usernameField: 'identifier'
@@ -17,14 +19,17 @@ passport.serializeUser((user, done) => {
     done(null, user.email);
 });
 
-passport.deserializeUser((email, done) => {
-    const user = {
-        fullname: 'Arnelle Balane',
-        username: 'arnellebalane',
-        email: 'arnellebalane@gmail.com',
-        avatar: 'static/images/default-avatar.png'
-    };
-    done(null, user);
+passport.deserializeUser(async (email, done) => {
+    const getUsersWithEmailQuery = await query('03-get-users-with-email.sql', { email });
+    const users = await db.query(getUsersWithEmailQuery);
+
+    if (users.rows.length > 0) {
+        const user = users.rows[0];
+        user.avatar = 'static/images/default-avatar.png';
+        done(null, users.rows[0]);
+    } else {
+        done(null, new Error('User not found.'));
+    }
 });
 
 module.exports = passport;
