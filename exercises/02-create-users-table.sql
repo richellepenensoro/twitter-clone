a2f45c8c981ad3faedbdcc2642716a6aa83b9ae0
richellepenensoro <rpenensoro06@gmail.com>
2017-10-31 01:52:26 -0700

    first commit

--- exercises/02-create-users-table.sql
+++ exercises/02-create-users-table.sql
@@ -6,3 +6,12 @@
 --     created_at   (timestamp)     default to current timestamp
 --
 -- Write your query below:
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+  fullname VARCHAR(30) NOT NULL,
+  username VARCHAR(15) UNIQUE NOT NULL,
+  email VARCHAR(30) PRIMARY KEY NOT NULL,
+  password VARCHAR(15) NOT NULL,
+  created_at TIMESTAMP DEFAULT now()
+);
