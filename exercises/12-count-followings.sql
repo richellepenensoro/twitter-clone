aef0bcf30491c70470e72c46160de6940ce97315
Arnelle Balane <arnellebalane@gmail.com>
2017-11-30 22:27:51 +0800

    add instructions for next batch of exercises

---
+++ exercises/12-count-followings.sql
@@ -0,0 +1,7 @@
+-- Count how many users the given user is following.
+--
+-- You have access to the following variables which can be used as placeholders
+-- for actual values:
+--     - email
+--
+-- Write your query below:
