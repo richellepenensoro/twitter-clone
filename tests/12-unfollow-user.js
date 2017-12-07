089777629931a7b06ec996d79c4f0147d1bc3cdc
Arnelle Balane <arnellebalane@gmail.com>
2017-12-07 23:51:48 +0800

    fix label typo for exercise 12

--- tests/12-unfollow-user.js
+++ tests/12-unfollow-user.js
@@ -5,7 +5,7 @@ const { query } = require('../utils');
 const exercisesDirectory = config.get('EXERCISES_DIRECTORY');
 const databaseConfig = require(`../${exercisesDirectory}/01-database-connection.json`);
 
-suite('11-follow-user', function() {
+suite('12-unfollow-user', function() {
     const dropTablesQuery = `
         DROP TABLE IF EXISTS follows;
         DROP TABLE IF EXISTS users;
